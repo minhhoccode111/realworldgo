6453ada081195ccabd2124433ae22bf54250579f
minhhoccode111 <minhhoccode111@gmail.com>
2025-10-11 13:30:08 +0700

    refactor cors

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -7,24 +7,36 @@ import (
 
 // CORS middleware
 func CorsMiddleware(AllowedOrigins []string) func(http.Handler) http.Handler {
+	allowAll := slices.Contains(AllowedOrigins, "*")
+
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			origin := r.Header.Get("Origin")
-			if slices.Contains(AllowedOrigins, "*") || slices.Contains(AllowedOrigins, origin) {
-				if slices.Contains(AllowedOrigins, "*") {
-					w.Header().Set("Access-Control-Allow-Origin", "*")
-					w.Header().Set("Access-Control-Allow-Credentials", "false")
-				} else {
-					w.Header().Set("Access-Control-Allow-Origin", origin)
-					w.Header().Set("Access-Control-Allow-Credentials", "true")
-				}
+
+			var allowed bool
+			if allowAll {
+				allowed = true
+				w.Header().Set("Access-Control-Allow-Origin", "*")
+			} else if slices.Contains(AllowedOrigins, origin) {
+				allowed = true
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+				w.Header().Set("Access-Control-Allow-Credentials", "true")
 			}
 
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
-			w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type")
+			if allowed {
+				w.Header().
+					Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
+				w.Header().
+					Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type")
+				// w.Header().Set("Access-Control-Max-Age", "86400") // 24 hours
+			}
 
 			if r.Method == http.MethodOptions {
-				w.WriteHeader(http.StatusNoContent)
+				if allowed {
+					w.WriteHeader(http.StatusNoContent)
+				} else {
+					w.WriteHeader(http.StatusForbidden)
+				}
 				return
 			}
 
